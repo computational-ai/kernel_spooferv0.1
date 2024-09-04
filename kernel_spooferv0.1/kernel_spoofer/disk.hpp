01cf1c48015fb54f0e88826f8bdd533a4f82c412
computational-ai <davidapex011@gmail.com>
2024-09-04 13:17:28 -0700

    Delete kernel_spooferv0.1/kernel_spoofer directory

--- kernel_spooferv0.1/kernel_spoofer/disk.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <ntddk.h>
-
-extern PDRIVER_DISPATCH PartControlOriginal;
-
-void SpoofDisks();
-NTSTATUS PartInfoIoc(PDEVICE_OBJECT device, PIRP irp, PVOID context);
-NTSTATUS PartControl(PDEVICE_OBJECT device, PIRP irp);
--- kernel_spooferv0.1/kernel_spoofer/utils.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <ntddk.h>
-
-void RandomizeSerial(char* buffer, size_t length);
-PBYTE FindPatternImage(PVOID base, const char* pattern, const char* mask);
-PVOID GetBaseAddress(const char* driverName);
